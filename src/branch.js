ef97f332425b318deeb70cbfc666a106497873ba
Jonathan Buhacoff <jonathan@buhacoff.net>
2019-09-12 05:49:26 -0700

    add: new `--update` option to `branch` command

--- src/branch.js
+++ src/branch.js
@@ -8,10 +8,16 @@ exports.command = 'branch <name>';
 exports.describe = 'create a new branch in each repository';
 exports.builder = function (yargs) {
     return yargs.positional('name', {
-        describe: 'new branch name'
+        describe: 'new branch name',
+        type: 'string'
     })
     .option('create', {
-        describe: 'create a new manifest of the same name'
+        describe: 'create a new manifest of the same name',
+        type: 'boolean'
+    })
+    .option('update', {
+        describe: 'update current manifest',
+        type: 'boolean'
     })
 };
 exports.handler = function (argv) {
@@ -24,7 +30,8 @@ exports.handler = function (argv) {
     var currentManifestName = libinfo.readCurrentManifestName();
     var content = libinfo.readManifestContent(currentManifestName);
     var repoList = Object.getOwnPropertyNames(content);
-    // check that we can operate on each repository (NOTE: code duplicated in status.js and merge.js)
+
+    // check that we can operate on each repository (NOTE: code duplicated in status.js and merge.js and update.js)
     var isVcsPluginAvailable = true;
     for(var i=0; i<repoList.length; i++) {
         var repo = repoList[i];
@@ -48,6 +55,8 @@ exports.handler = function (argv) {
     // compute new branch name in each repo
     var branchMap = {}; // repo => new branch name
     var isBranchCreated = false;
+
+    // TODO: do we really need this? we could just switch to existing new branch for that repo, user probably then wants to create same branch in all other repos too ; if we fail here then user has to do it one by one ...
     // check that new branch name does not already exist in any repo
     for(var i=0; i<repoList.length; i++) {
         var repo = repoList[i];
@@ -60,14 +69,28 @@ exports.handler = function (argv) {
     if( isBranchCreated ) {
         process.exit(1);
     }
+
     // create a branch in each repo and switch to it
     for(var i=0; i<repoList.length; i++) {
         var repo = repoList[i];
         var plugin = vcsMap[content[repo].vcs];
-        plugin.createBranch(repo, argv.name);
+
+        var newBranchName = argv.name;
+        if( newBranchName.startsWith('+')) {
+            var currentBranchName = plugin.getRef(repo);
+            newBranchName = currentBranchName.concat(newBranchName);
+        }
+
+        plugin.createBranch(repo, newBranchName);
+        content[repo].ref = newBranchName;
     }
 
-    // Create manifest file if --create flag is used
+    // update current manifest file if --update flag is specified
+    if( argv.update === true) {
+        libinfo.writeManifestContent(currentManifestName, content);
+    }
+
+    // create new manifest file if --create flag is specified
     if( argv.create === true) {
         var newBranchName = argv.name;
         if( newBranchName.startsWith('+')) {
@@ -76,7 +99,7 @@ exports.handler = function (argv) {
         else {
             newManifestName = newBranchName;
         }
-        libinfo.writeCurrentManifestName(newManifestName);
         libinfo.writeManifestContent(newManifestName, content);
+        libinfo.writeCurrentManifestName(newManifestName);
     }
 };
